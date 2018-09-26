2124c762c9f9a57802369a8bc6219f4973ccca96
D D Dhanveer <mail2dhans@gmail.com>
2018-09-26 12:18:14 +0900

    INITIAL

---
+++ myraw.R
@@ -0,0 +1,7 @@
+#setwd("/Users/dhanveer/Documents/online_learning/r")
+
+# git remote add origin git@github.com:dappaduppa/dhanveer_ci
+# git push -u origin master
+# git pull origin master
+
+apropos('mean')
